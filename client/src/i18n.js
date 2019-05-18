ecc9efba43877b504b06199e80ed8eacdcbb7be0
zmeu <zmeu@buc-ird-01b.whitehat.ro>
2019-05-19 00:17:15 +0300

    *

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -3,51 +3,11 @@ import { reactI18nextModule } from 'react-i18next';
 import { initReactI18n } from 'react-i18next/hooks';
 import langDetect from 'i18next-browser-languagedetector';
 
-import vi from './__locales/vi.json';
 import en from './__locales/en.json';
-import ru from './__locales/ru.json';
-import es from './__locales/es.json';
-import fr from './__locales/fr.json';
-import ja from './__locales/ja.json';
-import sv from './__locales/sv.json';
-import ptBR from './__locales/pt-br.json';
-import zhTW from './__locales/zh-tw.json';
-import bg from './__locales/bg.json';
-import zhCN from './__locales/zh-cn.json';
 
 const resources = {
     en: {
         translation: en,
-    },
-    vi: {
-        translation: vi,
-    },
-    ru: {
-        translation: ru,
-    },
-    es: {
-        translation: es,
-    },
-    fr: {
-        translation: fr,
-    },
-    ja: {
-        translation: ja,
-    },
-    sv: {
-        translation: sv,
-    },
-    'pt-BR': {
-        translation: ptBR,
-    },
-    'zh-TW': {
-        translation: zhTW,
-    },
-    bg: {
-        translation: bg,
-    },
-    'zh-CN': {
-        translation: zhCN,
     },
 };
 
